53237b1054caa700cc68660d0fb93d1264de012d
Daniel Nery <danielnso@outlook.com>
2017-06-30 14:19:31 -0300

    Improvements
    - Grid Layout on Strategy Selector
    - Hard coded devices (show only robots BTs)

--- src/pages/strategies/strategies.ts
+++ src/pages/strategies/strategies.ts
@@ -11,66 +11,46 @@ export class StrategiesPage {
   bt_connection: any;
   mac: string;
   strategies: Array<{name: string, n: number}>;
-  devices: Array<any>;
+
+  devices: Array<{name: string, mac: string}>;
 
   constructor(public navCtrl: NavController, private bt: BluetoothSerial, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private alertCtrl: AlertController) {
-    this.bt.enable().then(() => {
-      console.log("BT enabled!");
-    }, () => {
-      console.log("BT not enabled");
-    });
+    // this.bt.enable().then(() => {
+    //   console.log("BT enabled!");
+    // }, () => {
+    //   console.log("BT not enabled");
+    // });
 
-    this.bt.subscribe('\n').subscribe((data) => {
-      console.log("Received: " + data);
-    })
+    // this.bt.subscribe('\n').subscribe((data) => {
+    //   console.log("Received: " + data);
+    // })
 
     this.strategies = [];
-    for (let i = 1; i <= 22; i++) {
+    for (let i = 1; i <= 24; i++) {
       this.strategies.push(
-        { name: "Estrategia " + String(i), n: i }
+        { name: "Est " + String(i), n: i }
       );
     }
 
-    // this.strategies = [
-    //   { name: "Estratégia 1",  n:  1 },
-    //   { name: "Estratégia 2",  n:  2 },
-    //   { name: "Estratégia 3",  n:  3 },
-    //   { name: "Estratégia 4",  n:  4 },
-    //   { name: "Estratégia 5",  n:  5 },
-    //   { name: "Estratégia 6",  n:  6 },
-    //   { name: "Estratégia 7",  n:  7 },
-    //   { name: "Estratégia 8",  n:  8 },
-    //   { name: "Estratégia 9",  n:  9 },
-    //   { name: "Estratégia 10", n: 10 },
-    //   { name: "Estratégia 11", n: 11 },
-    //   { name: "Estratégia 12", n: 12 },
-    //   { name: "Estratégia 13", n: 13 },
-    //   { name: "Estratégia 14", n: 14 },
-    //   { name: "Estratégia 15", n: 15 },
-    //   { name: "Estratégia 16", n: 16 },
-    //   { name: "Estratégia 17", n: 17 },
-    //   { name: "Estratégia 18", n: 18 },
-    //   { name: "Estratégia 19", n: 19 },
-    //   { name: "Estratégia 20", n: 20 },
-    //   { name: "Estratégia 21", n: 21 },
-    //   { name: "Estratégia 22", n: 22 }
-    // ];
-
+    this.devices = [
+      { name: "Pepita",      mac: "00:80:E1:B7:EE:F0" },
+      { name: "HC06-MarcoW", mac: "20:16:05:19:17:79" }
+    ];
   }
 
   show_devices(): void {
-    this.bt.list().then((list) => {
-      console.log("Listing");
-      console.log(list);
+    // this.bt.list().then((list) => {
+      // console.log("Listing");
+      // console.log(list);
 
       let alert = this.alertCtrl.create();
-      alert.setTitle('Dispositivos Pareados');
+      alert.setTitle('BTs dos Robôs');
 
-      for (var dev of list) {
+      for (var dev of this.devices) {
         alert.addInput({
           type: 'radio',
           label: dev.name,
-          value: dev.address
+          value: dev.mac
         });
       }
 
@@ -85,12 +65,12 @@ export class StrategiesPage {
 
       alert.present();
 
-    }, (err) => {
-      this.toastCtrl.create({
-        message: 'Falha em listar dispositivos',
-        duration: 3000
-      }).present();
-    })
+    // }, (err) => {
+    //   this.toastCtrl.create({
+    //     message: 'Falha em listar dispositivos',
+    //     duration: 3000
+    //   }).present();
+    // })
   }
 
   connect(): void {
@@ -132,6 +112,6 @@ export class StrategiesPage {
       console.log("BT ok");
     }, (err) => {
       console.log(err);
-    })
+    });
   }
 }

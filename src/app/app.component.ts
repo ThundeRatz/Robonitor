acb99b19de9e0c4767726e0aac1ff1a2652ffab2
Daniel Nery <danielnso97@gmail.com>
2019-03-27 17:35:18 -0300

    [v2] Add blank Ionic 4 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,26 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { Component } from '@angular/core';
 
-import { HomePage } from '../pages/home/home';
-import { StrategiesPage } from '../pages/strategies/strategies';
-import { JoystickPage } from '../pages/joystick/joystick';
-import { MonitracerPage } from '../pages/monitracer/monitracer';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
-  templateUrl: 'app.html'
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
 })
-export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-
-  rootPage:any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private screenOrientation: ScreenOrientation) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      splashScreen.hide();
-      this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-    });
-
-    this.pages = [
-      { title: 'Home',              component: HomePage },
-      { title: 'Strategy Selector', component: StrategiesPage },
-      { title: 'Monitracer',        component: MonitracerPage },
-      { title: 'Joystick',          component: JoystickPage },
-    ];
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,23 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { StrategiesPage } from '../pages/strategies/strategies';
-import { JoystickPage } from '../pages/joystick/joystick';
-import { MonitracerPage } from '../pages/monitracer/monitracer';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    JoystickPage,
-    StrategiesPage,
-    MonitracerPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    JoystickPage,
-    StrategiesPage,
-    MonitracerPage
-  ],
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    BluetoothSerial,
-    ScreenOrientation,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
